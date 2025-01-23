72b61ba75a880b787fb006300dee970aae0e2b06
opli74 <gaggersgag@gmail.com>
2025-01-23 01:50:16 +0000

    added injection when process is already open, this happens automatically using NtQuerySystemInformation.

--- x64DetourLoader/entry.cpp
+++ x64DetourLoader/entry.cpp
@@ -1,5 +1,6 @@
-#include <Windows.h>
 #include <stdio.h>
+#include "pe.h"
+
 
 VOID LogError(
     LPCSTR str
@@ -10,48 +11,136 @@ VOID LogError(
     ExitProcess(1);
 }
 
+typedef NTSTATUS ( NTAPI* NtQuerySystemInformationDef ) (
+                                                        _In_      SYSTEM_INFORMATION_CLASS   SystemInformationClass,
+                                                        _Inout_   PVOID                      SystemInformation,
+                                                        _In_      DWORD                      SystemInformationLength,
+                                                        _Out_opt_ PDWORD                     ReturnLength
+                                                       );
+
+static NtQuerySystemInformationDef g_NtQuerySystemInformation = NULL;
+
+DWORD FindProcessPID(
+   CONST WCHAR* ProcessName
+)
+{
+    PLDR_DATA_TABLE_ENTRY NtDllImage = PeRetrieveImageBase(L"NTDLL.dll");
+
+    if ( !NtDllImage )
+    {
+        LogError("Failed to get NTDLL image");
+    }
+
+    g_NtQuerySystemInformation = (NtQuerySystemInformationDef)PeRetrieveImageExport(
+        NtDllImage->DllBase,
+        "NtQuerySystemInformation"
+    );
+
+    if ( !g_NtQuerySystemInformation )
+    {
+        LogError("Failed to get NtQuerySystemInformation address");
+    }
+
+
+    DWORD dwSize;
+    g_NtQuerySystemInformation(SystemExtendedProcessInformation, NULL, 0, &dwSize); // when size is null, it will return the required size
+
+    // allocate bit more memory just incase!
+    dwSize += 0x1000;
+
+    PVOID pBuffer = malloc( dwSize );
+    if ( !pBuffer )
+    {
+        LogError("Failed to allocate memory for process information");
+    }
+
+    NTSTATUS status = g_NtQuerySystemInformation(SystemExtendedProcessInformation, pBuffer, dwSize, &dwSize); // should return buffer of SYSTEM_PROCESS_INFORMATION in pBuffer
+
+    if ( status < 0 )
+    {
+        free(pBuffer);
+        LogError("NtQuerySystemInformation has failed");
+    }
+
+    PSYSTEM_PROCESS_INFORMATION pCurrent = (PSYSTEM_PROCESS_INFORMATION)pBuffer;
+
+    DWORD PID = 0;
+
+    do
+    {
+        pCurrent = (PSYSTEM_PROCESS_INFORMATION)(((PBYTE)pCurrent) + pCurrent->NextEntryOffset); // this is how you iterate through the list
+
+        if ( !pCurrent->ImageName.Buffer && !pCurrent->ImageName.Length )
+        {
+            continue;
+        }
+
+        if ( _wcsicmp( pCurrent->ImageName.Buffer, ProcessName ) == 0 )
+        {
+            PID = (DWORD)(ULONG_PTR)pCurrent->UniqueProcessId;
+            break;
+        }
+
+    } while ( pCurrent->NextEntryOffset );
+
+    free(pBuffer);
+    return PID;
+}
+
 int main(
 
 )
 {
-    CONST CHAR* DllBuffer = "C:/Users/Opli/source/repos/x64DetourDll/x64/Debug/x64DetourDll.dll";
-    CONST CHAR* ExeBuffer  = "C:/Users/Opli/source/repos/x64Detour/x64/Release/x64Detour.exe";
+    CONST CHAR* DllBuffer  = "C:/Users/Opli/source/repos/x64DetourDll/x64/Debug/x64DetourDll.dll";
+    CONST CHAR* ExeBuffer  = "C:/Users/Opli/source/repos/x64DetourTarget/x64/Release/x64DetourTarget.exe";
 
-    printf("Got full dll path: %s\n", DllBuffer);
-    printf("Got full exe path: %s\n", ExeBuffer);
-
-    STARTUPINFOA StartupInfo               = { };
-    PROCESS_INFORMATION ProcessInformation = { };
-
-    StartupInfo.cb = sizeof(StartupInfo);
-
-    BOOL result = CreateProcessA(
-        ExeBuffer,
-        NULL, 
-        NULL, 
-        NULL, 
-        FALSE, 
-        CREATE_NEW_CONSOLE,
-        NULL, 
-        NULL, 
-        &StartupInfo, 
-        &ProcessInformation
-    );
-
-    if (!result)
+    DWORD ProcessPID     = FindProcessPID(L"x64DetourTarget.exe");
+    HANDLE ProcessHandle = nullptr;
+    
+    if ( !ProcessPID )
     {
-        LogError("Failed to create process");
+        STARTUPINFOA StartupInfo               = { };
+        PROCESS_INFORMATION ProcessInformation = { };
+
+        StartupInfo.cb = sizeof( StartupInfo );
+
+        BOOL result = CreateProcessA(
+            ExeBuffer,
+            NULL,
+            NULL,
+            NULL,
+            FALSE,
+            CREATE_NEW_CONSOLE,
+            NULL,
+            NULL,
+            &StartupInfo,
+            &ProcessInformation
+        );
+
+        if ( !result )
+        {
+            LogError("Failed to create process");
+        }
+
+        if ( !ProcessInformation.hProcess || !ProcessInformation.hThread )
+        {
+            LogError("Failed to get process handle from CreateProcessA");
+        }
+
+        CloseHandle(ProcessInformation.hThread);
+        ProcessHandle = ProcessInformation.hProcess;
     }
-
-    if (!ProcessInformation.hProcess)
+    else
     {
-        printf("Failed to get process handle\n");
-        system("pause");
-        return 1;
+        ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessPID );
+        if (!ProcessHandle)
+        {
+            LogError("Failed to get process handle from NtQuerySystemInformation");
+        }
     }
 
     PVOID AllocBuffer = (PVOID)VirtualAllocEx(
-        ProcessInformation.hProcess,
+        ProcessHandle,
         NULL,
         MAX_PATH,
         MEM_COMMIT | MEM_RESERVE,
@@ -64,7 +153,7 @@ int main(
     }
 
     BOOL HasWritten = WriteProcessMemory(
-        ProcessInformation.hProcess,
+        ProcessHandle,
         AllocBuffer,
         DllBuffer,
         MAX_PATH,
@@ -76,7 +165,7 @@ int main(
         LogError("Failed to write memory in process");
     }
 
-    FARPROC LoadLibraryFn = GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA");
+    FARPROC LoadLibraryFn = GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA"); // change this to use the PE shit sometime soon?
 
     if (!LoadLibraryFn)
     {
@@ -84,7 +173,7 @@ int main(
     }
 
     HANDLE RemoteThread = CreateRemoteThread(
-        ProcessInformation.hProcess,
+        ProcessHandle,
         NULL,
         NULL,
         (LPTHREAD_START_ROUTINE)LoadLibraryFn,
@@ -95,14 +184,12 @@ int main(
 
     if (!RemoteThread)
     {
+        CloseHandle(RemoteThread);
         LogError("Failed to create remote thread");
     }
 
     CloseHandle(RemoteThread);
-    CloseHandle(ProcessInformation.hProcess);
-    CloseHandle(ProcessInformation.hThread);
-
-    system("pause");
-
+    CloseHandle(ProcessHandle);
+  
     return 0;
 }
